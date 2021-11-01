36f0cab153aab3f5cf491ab1ace363bd327e5d20
Leonardo Marinho <leonardo.marinho@everi.com>
2021-11-01 15:02:35 -0300

    Firt commit with some tests and uniqueid service

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,58 @@
+// The test consists basically to use the describe method passing two parameters
+
+import { UniqueIdService } from "../unique-id.service";
+
+// A description of the test and an function that contain the tests
+describe(UniqueIdService.name, () => {
+    // Here, inside the function we can create a lot of "its" functions
+    // each it function can test something, for example: conditions.
+
+    let service: UniqueIdService = null;
+
+    // Before execute each test, this function will recreate a new instance of UniqueIdService
+    // then, the tests will not be affected by other tests and its values
+    beforeEach(() => {
+      service = new UniqueIdService();
+    });
+
+    // We use the template string to update automatically the name of method // if it's change
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+      should generate id when called with prefix`, () => {
+
+        const id = service.generateUniqueIdWithPrefix('app');
+
+      expect(id.startsWith('app-')).toBeTrue();
+    });
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+      should not generate duplicate IDs when called multiple times`, () => {
+      const ids = new Set();
+
+      for (let i = 0; i < 50; i++) {
+        ids.add(service.generateUniqueIdWithPrefix('app'));
+      }
+
+      expect(ids.size).toBe(50);
+   });
+
+   it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+      should return the number of generated ids when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+   });
+
+
+   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+      should throw when called with empty prefix value`, () => {
+
+      const emptyValues = [null, undefined, ''];
+      // When we need a throw test, is necessary pass the function inside a other function
+      emptyValues.forEach(emptyValue => expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+        // Pass the element of list that we have a problem to generate a contextualization of the error
+        .withContext(`Empty value: ${emptyValue}`) 
+        .toThrow()
+      );
+  });
+});
